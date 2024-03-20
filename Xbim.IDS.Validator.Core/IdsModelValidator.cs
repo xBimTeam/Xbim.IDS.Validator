8227321982849dc7ff344668ee5def211d603e4b
Amir Emil <amir.emil@xbim.net>
2024-03-20 12:52:37 +0000

    Merged PR 2718: fix: remove duplicate entity checks

--- Xbim.IDS.Validator.Core/IdsModelValidator.cs
+++ Xbim.IDS.Validator.Core/IdsModelValidator.cs
@@ -151,7 +151,7 @@ namespace Xbim.IDS.Validator.Core
               
 
                 // Get the applicable items
-                IEnumerable<IPersistEntity> items = ModelBinder.SelectApplicableEntities(model, spec);
+                IEnumerable<IPersistEntity> items = ModelBinder.SelectApplicableEntities(model, spec).Distinct();
                 token.ThrowIfCancellationRequested();
                 logger.LogInformation("          Checking {count} applicable items", items.Count());
                 foreach (var item in items)
