5a5c06607dceef41de36028aecb8c8615855e850
Andy Ward <andy.ward@xbim.net>
2024-06-05 09:13:07 +0100

    Updated latest testcases as per IDS 1.0 release. Includes latest tolerance tests, and minor updates to conventions WIP: To fix up breaking changes/fails

--- Xbim.IDS.Validator.Tests.Common/IdsTestCaseRunner.cs
+++ Xbim.IDS.Validator.Tests.Common/IdsTestCaseRunner.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Text.RegularExpressions;
 using Xbim.Common;
 using Xbim.Common.Step21;
 using Xbim.IDS.Validator.Common;
@@ -96,7 +97,7 @@ namespace Xbim.IDS.Validator.Tests.Common
         {
             string testRoot = @$"TestCases/{facetFolder}";
             DirectoryInfo d = new DirectoryInfo(testRoot);
-            var idsFiles = d.GetFiles($"{testType}-*.ids", SearchOption.AllDirectories);
+            var idsFiles = d.GetFiles($"{testType}*.ids", SearchOption.AllDirectories);
             return idsFiles;
         }
 
